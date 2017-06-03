336458201ebc96128061ed9c6148faf7d4b35e2b
alexmitroff <alexmitroff@gmail.com>
2017-06-03 10:59:07 +0300

    Added all stuff from old version static, media, app and so on

---
+++ encyclopedia/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from encyclopedia.models import Section, Unit, Month
+# Register your models here.
+
+admin.site.register(Section)
+admin.site.register(Unit)
+admin.site.register(Month)
