02d57480c5c2e9a74257a2f10b470b451b0d9711
alexmitroff <alexmitroff@gmail.com>
2019-09-24 19:05:07 +0300

    Fix unit view

--- encyclopedia/views/unit.py
+++ encyclopedia/views/unit.py
@@ -1,25 +1,22 @@
-def unit(request, unit_id):
-    template_url = 'pages/unit.html'
-    unit = Unit.objects.get(pk=int(unit_id))
-    sect = unit.section
-    try:
-        u_next = Unit.objects.filter(section=sect).filter(id__gt=unit.id).order_by('id')[:1].get()
-    except:
-        u_next = None
-    try:
-        u_prev = Unit.objects.filter(section=sect).filter(id__lt=unit.id).order_by('-id')[:1].get()
-    except:
-        u_prev = None
-    months = Month.objects.all()
-    rarity_range = range(0, unit.rarity)
-    last = last_month(unit)
-    month = get_month()
-    var = {'unit': unit,
-           'next': u_next,
-           'prev': u_prev,
-           'months': months,
-           'rarity_range': rarity_range,
-           'mon': month,
-           'last': last}
-    request_context = RequestContext(request)
-    return render(request, template_url, var)
+from django.shortcuts import render
+
+from encyclopedia.models import Month, Unit
+from encyclopedia.views.encyclopedia import EncyclopediaView
+
+
+class UnitView(EncyclopediaView):
+
+    def get(self, request, pk):
+        unit = Unit.objects.filter(pk=pk).select_related('section').first()
+        next_unit = Unit.objects.filter(section=unit.section, id__gt=unit.id).order_by('id').first()
+        prev_unit = Unit.objects.filter(section=unit.section, id__lt=unit.id).order_by('id').last()
+        context = {
+            'mon': self.get_current_month(),
+            'months': Month.objects.all(),
+            'rarity_range': range(0, unit.rarity),
+            'unit': unit,
+            'next': next_unit,
+            'prev': prev_unit,
+            'last': self.is_last_month(unit)
+        }
+        return render(request, 'pages/unit.html', context)
