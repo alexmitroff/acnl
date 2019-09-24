59e9ee0e9f7cfe8a990d334b7270ac3d1980a7cd
alexmitroff <alexmitroff@gmail.com>
2019-09-24 15:46:19 +0300

    Remove tabs from sitemap

--- acnl/sitemap.py
+++ acnl/sitemap.py
@@ -1,44 +1,45 @@
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.contrib.sitemaps import Sitemap
 from encyclopedia.models import Unit
 from datetime import datetime as dt
 
+
 class ViewSitemap(Sitemap):
-	changefreq = 'monthly'
-	priority = 0.8
-	i18n = True
-	
-	def items(self):
-		#static pages
-		return ['last', 'monthly']
-	
-	def location(self, item):
-		return reverse(item)
+    changefreq = 'monthly'
+    priority = 0.8
+    i18n = True
+
+    def items(self):
+        # static pages
+        return ['last', 'monthly']
+
+    def location(self, item):
+        return reverse(item)
+
 
 class EncSitemap(Sitemap):
-	changefreq = 'yearly'
-	priority = 0.5
-	i18n = True
-	
-	def items(self):
-		#static pages
-		return ['index']
-	
-	def location(self, item):
-		return reverse(item)
+    changefreq = 'yearly'
+    priority = 0.5
+    i18n = True
+
+    def items(self):
+        # static pages
+        return ['index']
+
+    def location(self, item):
+        return reverse(item)
 
 
-		
 class UnitSitemap(Sitemap):
-	changefreq = 'yearly'
-	priority = 0.5
-	i18n = False
-	
-	def items(self):
-		return Unit.objects.all().order_by('pos')
-	
-	def lastmod(self, obj):
-		return dt.now()
-	
-	def location(self, obj):
-		return "/encyclopedia/%s/" % obj.id		
+    changefreq = 'yearly'
+    priority = 0.5
+    i18n = False
+
+    def items(self):
+        return Unit.objects.all().order_by('pos')
+
+    def lastmod(self, obj):
+        return dt.now()
+
+    def location(self, obj):
+        return "/encyclopedia/%s/" % obj.id
