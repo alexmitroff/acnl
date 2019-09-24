20fd4cba0c7320746e65a8f0de06ae81107f334a
alexmitroff <alexmitroff@gmail.com>
2019-09-24 18:51:04 +0300

    Fix last view

--- encyclopedia/views/last.py
+++ encyclopedia/views/last.py
@@ -1,23 +1,18 @@
+from django.shortcuts import render
+
+from encyclopedia.views.encyclopedia import EncyclopediaView
 
 
+class LastView(EncyclopediaView):
 
-
-
-
-
-
-def last(request):
-    template_url = 'pages/last.html'
-    sections = Section.objects.filter(show=True).order_by('pos')
-    units = last_list(sections)
-    bugs = units[0]
-    fish = units[1]
-    deep = units[2]
-    month = get_month()
-    var = {'sections': sections,
-           'bugs': bugs,
-           'fish': fish,
-           'deep': deep,
-           'mon': month, }
-    request_context = RequestContext(request)
-    return render(request, template_url, var)
+    def get(self, request):
+        sections = self.get_shown_sections()
+        units = self.last_month_unit_list(sections)
+        context = {
+            'sections': sections,
+            'mon': self.get_current_month(),
+            'bugs': units[0],
+            'fish': units[1],
+            'deep': units[2],
+        }
+        return render(request, 'pages/last.html', context)
