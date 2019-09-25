7847675d389c254c5c9abdec1925b62742ea8f1b
alexmitroff <alexmitroff@gmail.com>
2019-09-25 19:58:47 +0300

    Refactor EncyclopediaView methods

--- encyclopedia/views/encyclopedia.py
+++ encyclopedia/views/encyclopedia.py
@@ -23,56 +23,62 @@ class EncyclopediaView(View):
 
     @staticmethod
     def get_units_from_section(section):
-        return section.unit_set.all()
+        return section.unit_set.all().order_by('pos').prefetch_related('months')
 
     @staticmethod
     def get_active_units_from_section(section):
         return section.unit_set.filter(months__pos=get_current_month_number())
 
-    def is_last_month(self, item):
-        current_month = self.get_month_by_position(get_current_month_number())
-        position = current_month.pos + 1
-        if position > 12:
-            position = 1
-        next_month = self.get_month_by_position(position)
-
-        months = item.months.all().order_by('pos')
-
-        if current_month not in months:
+    @staticmethod
+    def is_last_month(item):
+        current_month = get_current_month_number()
+        next_month = current_month + 1 if current_month < 12 else 1
+        if not item.months.filter(pos=current_month).exists():
             return False
         else:
-            if next_month not in months:
-                return True
-            return False
+            return not item.months.filter(pos=next_month).exists()
 
-    def is_first_month(self, item):
-        current_month = self.get_month_by_position(get_current_month_number())
-        position = current_month.pos - 1
-        if position < 1:
-            position = 12
-        prev_month = self.get_month_by_position(position)
-
-        months = item.months.all().order_by('pos')
-
-        if current_month not in months:
+    @staticmethod
+    def is_first_month(item):
+        current_month = get_current_month_number()
+        prev_month = current_month - 1 if current_month > 1 else 12
+        if not item.months.filter(pos=current_month).exists():
             return False
         else:
-            if prev_month not in months:
-                return True
-            return False
+            return not item.months.filter(pos=prev_month).exists()
 
     def get_active_units_list_by_section(self, sections):
         result = []
         for section in sections:
             active_units = list(self.get_active_units_from_section(section))
+            new_units = []
+            old_units = []
             for item in active_units:
                 if self.is_first_month(item):
                     item.banner = 'new'
+                    new_units.append(item)
+                else:
+                    old_units.append(item)
+
             result.append({
                 'name': section.name,
-                'units': active_units
+                'units': new_units+old_units,
             })
         return result
+
+    def get_last_chance_units_list_by_section(self, sections):
+        result = []
+        for section in sections:
+            active_units = list(self.get_active_units_from_section(section))
+            last_chance_units = [item for item in active_units if self.is_last_month(item)]
+            result.append({
+                'name': section.name,
+                'units': last_chance_units
+            })
+        return result
+
+
+
 
     def last_month_unit_list(self, sections):
         l_list = []
