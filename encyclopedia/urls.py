6d78a73a7052f16e33506c074bef7dc2d68a1d0b
alexmitroff <alexmitroff@gmail.com>
2019-09-25 20:05:45 +0300

    Rename index view to EncyclopediaView

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from encyclopedia.views.index import IndexView, NewIndexView
+from encyclopedia.views.encyclopedia import IndexView, EncyclopediaView
 from encyclopedia.views.last import LastView
 from encyclopedia.views.monthly import MonthlyView
 from encyclopedia.views.unit import UnitView
@@ -10,7 +10,7 @@ from encyclopedia.views.lastchanceunits import LastChanceUnitsView
 urlpatterns = [
     path('', LastChanceUnitsView.as_view(), name='last-chance-units'),
     path('available/', AvailableUnitsView.as_view(), name='available-units'),
-    path('encyclopedia/', NewIndexView.as_view(), name='encyclopedia'),
+    path('encyclopedia/', EncyclopediaView.as_view(), name='encyclopedia'),
     path('old/', LastView.as_view(), name='last'),
     path('old/available/', MonthlyView.as_view(), name='monthly'),
     path('old/encyclopedia/', IndexView.as_view(), name='index'),
