2c4fdf337fcac004f572131b451822289f779609
alexmitroff <alexmitroff@gmail.com>
2019-09-25 20:09:30 +0300

    Remove old last chance page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from encyclopedia.views.availableunits import AvailableUnitsView
 from encyclopedia.views.encyclopedia import EncyclopediaView
-from encyclopedia.views.last import LastView
 from encyclopedia.views.lastchanceunits import LastChanceUnitsView
 from encyclopedia.views.monthly import MonthlyView
 from encyclopedia.views.unit import UnitView
@@ -11,7 +10,6 @@ urlpatterns = [
     path('', LastChanceUnitsView.as_view(), name='last-chance-units'),
     path('available/', AvailableUnitsView.as_view(), name='available-units'),
     path('encyclopedia/', EncyclopediaView.as_view(), name='encyclopedia'),
-    path('old/', LastView.as_view(), name='last'),
     path('old/available/', MonthlyView.as_view(), name='monthly'),
     path('encyclopedia/<int:pk>/', UnitView.as_view(), name='unit'),
 ]
