7d7db449b0cbb24b6d0be3bef09bf3ade0999527
alexmitroff <alexmitroff@gmail.com>
2019-09-27 17:16:00 +0300

    Rename templates and old unit view

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from encyclopedia.views.availableunits import AvailableUnitsView
 from encyclopedia.views.encyclopedia import EncyclopediaView
 from encyclopedia.views.lastchanceunits import LastChanceUnitsView
-from encyclopedia.views.unit import UnitView
 
 app_name = 'encyclopedia'
 
@@ -11,5 +10,4 @@ urlpatterns = [
     path('', LastChanceUnitsView.as_view(), name='last-chance-units'),
     path('available/', AvailableUnitsView.as_view(), name='available-units'),
     path('encyclopedia/', EncyclopediaView.as_view(), name='encyclopedia'),
-    path('encyclopedia/<int:pk>/', UnitView.as_view(), name='unit'),
 ]
