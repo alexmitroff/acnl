5c667a7a7ec18891ddc2832392dee1a53c0065ea
alexmitroff <alexmitroff@gmail.com>
2021-05-30 00:41:38 +0300

    Update docker-compose pt3

--- acnl/settings/dev.py
+++ acnl/settings/dev.py
@@ -2,7 +2,7 @@ from acnl.settings.base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'acnl.mitroff.ru', '94.103.82.243']
 
 DATABASES = {
     'default': {
