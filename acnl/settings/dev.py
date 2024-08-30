c69a85a838e7ac56aff0c774678fefbe190444c1
alexmitroff <alexmitroff@gmail.com>
2024-08-30 17:22:00 +0200

    [develop] Setup local docker

--- acnl/settings/dev.py
+++ acnl/settings/dev.py
@@ -1,3 +1,3 @@
 from acnl.settings.base import *
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'acnl.mitroff.ru', '94.103.82.243']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
---
+++ deploy/django/dev-run.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Django container is ready!"
+echo "please run 'docker exec -it acnl_local_django bash' to access it"
+sleep infinity
