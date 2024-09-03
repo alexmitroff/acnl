245cf01113be2f6c7bccb47d921f4e7ba5078373
alexmitroff <alexmitroff@gmail.com>
2024-09-03 22:38:05 +0000

    [creatures] Refactor month list on creature detail page

--- creatures/views/creature.py
+++ creatures/views/creature.py
@@ -6,7 +6,6 @@ from django.db.models import Exists, OuterRef
 from creatures.models.animal import Animal
 from encyclopedia.core.month import month
 from encyclopedia.models import Month
-from encyclopedia.views.encyclopediabase import EncyclopediaBase
 
 
 class CreatureView(TemplateView):
@@ -24,6 +23,7 @@ class CreatureView(TemplateView):
 
         context.update({
             'mon': month.get_name(timezone.now()),
+            'mon_num': month.current,
             'months': months,
             'creature': creature,
             'next': creature.next,
@@ -32,24 +32,3 @@ class CreatureView(TemplateView):
             'page_title': creature.name,
         })
         return context
-
-    # def get(self, request, section_slug, creature_slug):
-    #     creature = get_object_or_404(Creature, slug=creature_slug)
-    #     next_creature = Creature.objects.filter(section__slug=section_slug,
-    #                                             position__gt=creature.position).order_by('position').first()
-    #     prev_creature = Creature.objects.filter(section__slug=section_slug,
-    #                                             position__lt=creature.position).order_by('position').last()
-    #     months = Month.objects.all().select_related('season').order_by('pos')
-    #     months = self.mark_active_month(months, creature)
-    #     context = {
-    #         'mon': self.get_current_month(),
-    #         'months': months,
-    #         'rarity_range': range(0, creature.rarity.level),
-    #         'creature': creature,
-    #         'next': next_creature,
-    #         'prev': prev_creature,
-    #         'last': self.is_last_month(creature),
-    #         # Additional
-    #         'page_title': creature.name,
-    #     }
-    #     return render(request, 'pages/creature.html', context)
